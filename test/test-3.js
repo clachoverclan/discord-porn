5d522206d846f409a67ad0b22227d368c8058bbb
C1ach0 <clachover.clan@gmail.com>
2022-07-31 22:17:55 +0200

    Delete test directory

--- test/test-3.js
+++
@@ -1,8 +0,0 @@
-const { RandomP } = require('../discord-porn');
-
-const nsfw = new RandomP();
-
-console.log(nsfw.categories)
-for(let category of nsfw.categories){
-    console.log(category, ":", nsfw.db[category].length);
-}
